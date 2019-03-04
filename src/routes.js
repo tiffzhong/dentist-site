56d23e6cce03ad88a416a0290a3b071bb18d673c
Tiffany Zhong <tiffzhong@gmail.com>
2019-03-03 23:21:49 -0800

    site setup

---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage/LandingPage";
+export default (
+  <Switch>
+    <Route exact path="/" component={LandingPage} />
+  </Switch>
+);
